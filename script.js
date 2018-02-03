98650c18ba4a82360bdd8e85ad3b5dde5a7197ff
Simon <homer12345@gmail.com>
2018-02-02 22:26:49 -0800

    Add missing merc missions

--- script.js
+++ script.js
@@ -31,108 +31,401 @@ function toggleElement(e) {
     saveStore();
 }
 
-var tables = ["quests","quests-dlc","quests-blade","blades","shopdeeds","uniquemonsters","mercmissions","secretareas","expman","bspon","foorara","doubloons"];
+var tables = ["quests", "quests-dlc", "quests-blade", "blades", "shopdeeds", "uniquemonsters", "mercmissions", "secretareas", "expman", "bspon", "foorara", "doubloons"];
 var defs = {
     "quests-fil": true,
-    "quests-col":
-    [{field:'name',title:'Name',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'area',title:'Area',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'location',title:'Location',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'giver',title:'Giver',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'prereqs',title:'Prerequisites',sortable:false,filterControl:'input',filterStrictSearch:false}],
+    "quests-col": [{
+            field: 'name',
+            title: 'Name',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'area',
+            title: 'Area',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'location',
+            title: 'Location',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'giver',
+            title: 'Giver',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'prereqs',
+            title: 'Prerequisites',
+            sortable: false,
+            filterControl: 'input',
+            filterStrictSearch: false
+        }
+    ],
     "quests-dlc-fil": true,
-    "quests-dlc-col":
-    [{field:'name',title:'Name',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'location',title:'Location',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'prereqs',title:'Prerequisites',sortable:false,filterControl:'input',filterStrictSearch:false},
-    {field:'description',title:'Description',sortable:false}],
+    "quests-dlc-col": [{
+            field: 'name',
+            title: 'Name',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'location',
+            title: 'Location',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'prereqs',
+            title: 'Prerequisites',
+            sortable: false,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'description',
+            title: 'Description',
+            sortable: false
+        }
+    ],
     "quests-blade-fil": true,
-    "quests-blade-col":
-    [{field:'name',title:'Name',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'blade',title:'Blade',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'location',title:'Location',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'prereqs',title:'Prerequisites',sortable:false,filterControl:'input',filterStrictSearch:false},
-    {field:'description',title:'Description',sortable:false}],
+    "quests-blade-col": [{
+            field: 'name',
+            title: 'Name',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'blade',
+            title: 'Blade',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'location',
+            title: 'Location',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'prereqs',
+            title: 'Prerequisites',
+            sortable: false,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'description',
+            title: 'Description',
+            sortable: false
+        }
+    ],
     "blades-fil": true,
-    "blades-col":
-    [{field:'blade',title:'Blade',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'role',title:'Role',sortable:true,filterControl:'select',filterStrictSearch:true},
-    {field:'stat',title:'Stat Modifier',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'weapon',title:'Weapon',sortable:true,filterControl:'select',filterStrictSearch:true},
-    {field:'element',title:'Element',sortable:true,filterControl:'select',filterStrictSearch: true},
-    {field:'acquisition',title:'Acquisition',sortable:true,filterControl:'input',filterStrictSearch:false}],
+    "blades-col": [{
+            field: 'blade',
+            title: 'Blade',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'role',
+            title: 'Role',
+            sortable: true,
+            filterControl: 'select',
+            filterStrictSearch: true
+        },
+        {
+            field: 'stat',
+            title: 'Stat Modifier',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'weapon',
+            title: 'Weapon',
+            sortable: true,
+            filterControl: 'select',
+            filterStrictSearch: true
+        },
+        {
+            field: 'element',
+            title: 'Element',
+            sortable: true,
+            filterControl: 'select',
+            filterStrictSearch: true
+        },
+        {
+            field: 'acquisition',
+            title: 'Acquisition',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        }
+    ],
     "shopdeeds-fil": true,
-    "shopdeeds-col":
-    [{field:'shop',title:'Shop',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'area',title:'Area',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'benefit',title:'Benefit',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'numitems',title:'# of Items Sold',sortable:true,filterControl:'select',filterStrictSearch:true},
-    {field:'merc',title:'Required Merc Mission(s)',sortable:true,filterControl:'input',filterStrictSearch:false}],
+    "shopdeeds-col": [{
+            field: 'shop',
+            title: 'Shop',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'area',
+            title: 'Area',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'benefit',
+            title: 'Benefit',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'numitems',
+            title: '# of Items Sold',
+            sortable: true,
+            filterControl: 'select',
+            filterStrictSearch: true
+        },
+        {
+            field: 'merc',
+            title: 'Required Merc Mission(s)',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        }
+    ],
     "uniquemonsters-fil": true,
-    "uniquemonsters-col":
-    [{field:'name',title:'Name',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'level',title:'Level',sortable:true,filterControl:'select',filterStrictSearch:true},
-    {field:'species',title:'Species',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'nation',title:'Nation',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'location',title:'Location',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'spawntime',title:'Spawntime',sortable:true,filterControl:'input',filterStrictSearch:false}],
+    "uniquemonsters-col": [{
+            field: 'name',
+            title: 'Name',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'level',
+            title: 'Level',
+            sortable: true,
+            filterControl: 'select',
+            filterStrictSearch: true
+        },
+        {
+            field: 'species',
+            title: 'Species',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'nation',
+            title: 'Nation',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'location',
+            title: 'Location',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'spawntime',
+            title: 'Spawntime',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        }
+    ],
     "mercmissions-fil": true,
-    "mercmissions-col":
-    [{field:'name',title:'Name',sortable:true,filterControl:'input',filterStrictSearch:false},
-    {field:'area',title:'Area',sortable:true,filterControl:'input',filterStrictSearch:false}],
+    "mercmissions-col": [{
+            field: 'name',
+            title: 'Name',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'area',
+            title: 'Area',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        },
+        {
+            field: 'notes',
+            title: 'Notes',
+            sortable: true,
+            filterControl: 'input',
+            filterStrictSearch: false
+        }
+    ],
     "secretareas-fil": false,
-    "secretareas-col":
-    [{field:'name',title:'Name',sortable:true},
-    {field:'location',title:'Location',sortable:true},
-    {field:'description',title:'Description'}],
+    "secretareas-col": [{
+            field: 'name',
+            title: 'Name',
+            sortable: true
+        },
+        {
+            field: 'location',
+            title: 'Location',
+            sortable: true
+        },
+        {
+            field: 'description',
+            title: 'Description'
+        }
+    ],
     "expman-fil": false,
-    "expman-col":
-    [{field:'name',title:'Name',sortable:true},
-    {field:'location',title:'Location',sortable:true},
-    {field:'mark',title:'Marker',sortable:true},
-    {field:'description',title:'Description'}],
+    "expman-col": [{
+            field: 'name',
+            title: 'Name',
+            sortable: true
+        },
+        {
+            field: 'location',
+            title: 'Location',
+            sortable: true
+        },
+        {
+            field: 'mark',
+            title: 'Marker',
+            sortable: true
+        },
+        {
+            field: 'description',
+            title: 'Description'
+        }
+    ],
     "bspon-fil": false,
-    "bspon-col":
-    [{field:'id',title:'#',sortable:true},
-    {field:'location',title:'Location',sortable:true},
-    {field:'mark',title:'Marker',sortable:true},
-    {field:'description',title:'Description'}],
+    "bspon-col": [{
+            field: 'id',
+            title: '#',
+            sortable: true
+        },
+        {
+            field: 'location',
+            title: 'Location',
+            sortable: true
+        },
+        {
+            field: 'mark',
+            title: 'Marker',
+            sortable: true
+        },
+        {
+            field: 'description',
+            title: 'Description'
+        }
+    ],
     "foorara-fil": false,
-    "foorara-col":
-    [{field:'id',title:'#'},
-    {field:'location',title:'Location'},
-    {field:'mark',title:'Marker'},
-    {field:'description',title:'Description'}],
+    "foorara-col": [{
+            field: 'id',
+            title: '#'
+        },
+        {
+            field: 'location',
+            title: 'Location'
+        },
+        {
+            field: 'mark',
+            title: 'Marker'
+        },
+        {
+            field: 'description',
+            title: 'Description'
+        }
+    ],
     "doubloons-fil": false,
-    "doubloons-col":
-    [{field:'id',title:'#',sortable:true},
-    {field:'location',title:'Location',sortable:true},
-    {field:'mark',title:'Marker',sortable:true},
-    {field:'description',title:'Description'}],
-    "trust-col":
-    [{field:'name',title:'Name',sortable:true},
-    {field:'category',title:'Category',sortable:true},
-    {field:'trust',title:'Trust',sortable:true},
-    {field:'location',title:'Location',sortable:true},
-    {field:'shop',title:'Shop',sortable:true},
-    {field:'cost',title:'Cost (at Dev Lv.5)',sortable:true}]
+    "doubloons-col": [{
+            field: 'id',
+            title: '#',
+            sortable: true
+        },
+        {
+            field: 'location',
+            title: 'Location',
+            sortable: true
+        },
+        {
+            field: 'mark',
+            title: 'Marker',
+            sortable: true
+        },
+        {
+            field: 'description',
+            title: 'Description'
+        }
+    ],
+    "trust-col": [{
+            field: 'name',
+            title: 'Name',
+            sortable: true
+        },
+        {
+            field: 'category',
+            title: 'Category',
+            sortable: true
+        },
+        {
+            field: 'trust',
+            title: 'Trust',
+            sortable: true
+        },
+        {
+            field: 'location',
+            title: 'Location',
+            sortable: true
+        },
+        {
+            field: 'shop',
+            title: 'Shop',
+            sortable: true
+        },
+        {
+            field: 'cost',
+            title: 'Cost (at Dev Lv.5)',
+            sortable: true
+        }
+    ]
 };
 
 $(function() {
     var data;
-    $.getJSON('data.min.json', function(j){
+    $.getJSON('data.min.json', function(j) {
         data = j;
 
         // craft tables
         for (let tab of tables) {
             (function(t) {
-                $('#'+t+'-table').bootstrapTable({
+                $('#' + t + '-table').bootstrapTable({
                     data: data[t],
                     striped: true,
                     uniqueId: "uid",
-                    filterControl: defs[t+"-fil"],
+                    filterControl: defs[t + "-fil"],
                     hideUnusedSelectOptions: true,
-                    columns: defs[t+"-col"],
+                    columns: defs[t + "-col"],
                     onClickRow: function(row, $element, field) {
                         toggleElement($element);
                     },
